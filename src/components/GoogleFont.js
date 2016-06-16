2e66aa1262846470cb971006110a1d87abc31862
Kyle Mathews <mathews.kyle@gmail.com>
2016-06-15 18:42:15 -0700

    Check that options declares Google Fonts before building component

--- src/components/GoogleFont.js
+++ src/components/GoogleFont.js
@@ -8,23 +8,27 @@ module.exports = (options: any) =>
     render () {
       // Create family + styles string
       let fontsStr = ''
-      const fonts = options.googleFonts.map((font) => {
-        let str = ''
-        str += font.name.split(' ').join('+')
-        str += ':'
-        str += font.styles.join(',')
+      if (options.googleFonts) {
+        const fonts = options.googleFonts.map((font) => {
+          let str = ''
+          str += font.name.split(' ').join('+')
+          str += ':'
+          str += font.styles.join(',')
 
-        return str
-      })
-
-      fontsStr = fonts.join('|')
-
-      if (fontsStr) {
-        return React.DOM.link({
-          href: `//fonts.googleapis.com/css?family=${fontsStr}`,
-          rel: 'stylesheet',
-          type: 'text/css',
+          return str
         })
+
+        fontsStr = fonts.join('|')
+
+        if (fontsStr) {
+          return React.DOM.link({
+            href: `//fonts.googleapis.com/css?family=${fontsStr}`,
+            rel: 'stylesheet',
+            type: 'text/css',
+          })
+        } else {
+          return null
+        }
       } else {
         return null
       }
