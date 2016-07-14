f3f730956db2b9522395f65ed436e600a55361bb
Kyle Mathews <mathews.kyle@gmail.com>
2016-07-14 15:54:10 -0700

    Add new 'Sutro' theme

---
+++ packages/typography-theme-sutro/src/index.js
@@ -0,0 +1,94 @@
+// @flow
+import gray from 'gray-percentage'
+import type { OptionsType } from 'Types'
+import { MOBILE_MEDIA_QUERY, TABLET_MEDIA_QUERY } from 'typography-breakpoint-constants'
+
+const theme: OptionsType = {
+  baseFontSize: '18px',
+  baseLineHeight: '32px',
+  googleFonts: [
+    {
+      name: 'Open Sans',
+      styles: [
+        '700',
+      ],
+    },
+    {
+      name: 'Merriweather',
+      styles: [
+        '300',
+        '300i',
+        '700',
+        '700i',
+      ],
+    },
+  ],
+  headerFontFamily: ['Open Sans', 'sans-serif'],
+  bodyFontFamily: ['Merriweather', 'Georgia', 'serif'],
+  headerGray: 10,
+  bodyGray: 10,
+  headerWeight: 700,
+  bodyWeight: 300,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options) => ({
+    a: {
+      color: '#f92300',
+      textDecoration: 'none',
+    },
+    'a:hover,a:active': {
+      textDecoration: 'underline',
+    },
+    blockquote: {
+      ...adjustFontSizeTo('21px'),
+      color: gray(41),
+      fontStyle: 'italic',
+      paddingLeft: rhythm(13/16),
+      marginLeft: 0,
+      borderLeft: `${rhythm(3/16)} solid ${gray(80)}`,
+    },
+    'blockquote > :last-child': {
+      marginBottom: 0,
+    },
+    'blockquote cite': {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: gray(options.bodyGray),
+      fontStyle: options.bodyWeight,
+    },
+    'blockquote cite:before': {
+      content: '"— "',
+    },
+    ul: {
+      listStyle: 'disc',
+    },
+    'ul,ol': {
+      marginLeft: 0,
+    },
+    [MOBILE_MEDIA_QUERY]: {
+      'ul,ol': {
+        marginLeft: rhythm(1),
+      },
+      blockquote: {
+        marginLeft: rhythm(-3/4),
+        marginRight: 0,
+        paddingLeft: rhythm(9/16),
+      },
+    },
+    [TABLET_MEDIA_QUERY]: {
+      h1: {
+        ...adjustFontSizeTo('36px'),
+      },
+    },
+    'h1,h2,h3,h4,h5,h6': {
+      marginTop: rhythm(2),
+    },
+    h1: {
+      ...adjustFontSizeTo('48px'),
+      letterSpacing: '-2px',
+    },
+    h6: {
+      fontStyle: 'italic',
+    },
+  }),
+}
+
+export default theme
