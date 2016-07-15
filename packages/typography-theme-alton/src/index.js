52fd5b4fad86aa8f8e28ff8999eaa580bea7de89
Kyle Mathews <mathews.kyle@gmail.com>
2016-07-14 19:35:43 -0700

    Reduce font size slightly on mobile for Alton theme

--- packages/typography-theme-alton/src/index.js
+++ packages/typography-theme-alton/src/index.js
@@ -2,6 +2,7 @@
 import gray from 'gray-percentage'
 import type { OptionsType } from 'Types'
 import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants'
+import verticalRhythm from 'compass-vertical-rhythm'
 
 const theme: OptionsType = {
   baseFontSize: '18px',
@@ -30,41 +31,51 @@ const theme: OptionsType = {
   headerWeight: 700,
   bodyWeight: 400,
   boldWeight: 700,
-  overrideStyles: ({ adjustFontSizeTo, rhythm }, options) => ({
-    a: {
-      color: '#ff5700',
-      textDecoration: 'none',
-    },
-    'a:hover,a:active': {
-      color: gray(options.bodyGray),
-    },
-    blockquote: {
-      ...adjustFontSizeTo('21px'),
-      color: gray(41),
-      fontStyle: 'italic',
-      paddingLeft: rhythm(13/16),
-      marginLeft: 0,
-      borderLeft: `${rhythm(3/16)} solid ${gray(10)}`,
-    },
-    'blockquote > :last-child': {
-      marginBottom: 0,
-    },
-    'blockquote cite': {
-      ...adjustFontSizeTo(options.baseFontSize),
-      color: gray(options.bodyGray),
-      fontStyle: options.bodyWeight,
-    },
-    'blockquote cite:before': {
-      content: '"— "',
-    },
-    [MOBILE_MEDIA_QUERY]: {
-      blockquote: {
-        marginLeft: rhythm(-3/4),
-        marginRight: 0,
-        paddingLeft: rhythm(9/16),
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options) => {
+    const vr = verticalRhythm({
+      baseFontSize: '16px',
+      baseLineHeight: '28.44px',
+    })
+
+    return {
+      a: {
+        color: '#ff5700',
+        textDecoration: 'none',
       },
-    },
-  }),
+      'a:hover,a:active': {
+        color: gray(options.bodyGray),
+      },
+      blockquote: {
+        ...adjustFontSizeTo('21px'),
+        color: gray(41),
+        fontStyle: 'italic',
+        paddingLeft: rhythm(13/16),
+        marginLeft: 0,
+        borderLeft: `${rhythm(3/16)} solid ${gray(10)}`,
+      },
+      'blockquote > :last-child': {
+        marginBottom: 0,
+      },
+      'blockquote cite': {
+        ...adjustFontSizeTo(options.baseFontSize),
+        color: gray(options.bodyGray),
+        fontStyle: options.bodyWeight,
+      },
+      'blockquote cite:before': {
+        content: '"— "',
+      },
+      [MOBILE_MEDIA_QUERY]: {
+        html: {
+          ...vr.establishBaseline(),
+        },
+        blockquote: {
+          marginLeft: rhythm(-3/4),
+          marginRight: 0,
+          paddingLeft: rhythm(9/16),
+        },
+      },
+    }
+  },
 }
 
 export default theme
