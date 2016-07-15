f28ea8ffa7088c527016a51beeeeb184eb3a8788
Kyle Mathews <mathews.kyle@gmail.com>
2016-07-14 19:21:09 -0700

    Add new 'Alton' theme

---
+++ packages/typography-theme-alton/src/index.js
@@ -0,0 +1,70 @@
+// @flow
+import gray from 'gray-percentage'
+import type { OptionsType } from 'Types'
+import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants'
+
+const theme: OptionsType = {
+  baseFontSize: '18px',
+  baseLineHeight: '32px',
+  googleFonts: [
+    {
+      name: 'Domine',
+      styles: [
+        '700',
+      ],
+    },
+    {
+      name: 'Open Sans',
+      styles: [
+        '400',
+        '400i',
+        '700',
+        '700i',
+      ],
+    },
+  ],
+  headerFontFamily: ['Domine', 'sans-serif'],
+  bodyFontFamily: ['Open Sans', 'sans-serif'],
+  headerGray: 0,
+  bodyGray: 0,
+  headerWeight: 700,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options) => ({
+    a: {
+      color: '#ff5700',
+      textDecoration: 'none',
+    },
+    'a:hover,a:active': {
+      color: gray(options.bodyGray),
+    },
+    blockquote: {
+      ...adjustFontSizeTo('21px'),
+      color: gray(41),
+      fontStyle: 'italic',
+      paddingLeft: rhythm(13/16),
+      marginLeft: 0,
+      borderLeft: `${rhythm(3/16)} solid ${gray(10)}`,
+    },
+    'blockquote > :last-child': {
+      marginBottom: 0,
+    },
+    'blockquote cite': {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: gray(options.bodyGray),
+      fontStyle: options.bodyWeight,
+    },
+    'blockquote cite:before': {
+      content: '"— "',
+    },
+    [MOBILE_MEDIA_QUERY]: {
+      blockquote: {
+        marginLeft: rhythm(-3/4),
+        marginRight: 0,
+        paddingLeft: rhythm(9/16),
+      },
+    },
+  }),
+}
+
+export default theme
