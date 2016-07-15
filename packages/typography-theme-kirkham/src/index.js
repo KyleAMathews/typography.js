081808cfe2b815b00b612df368eac73fcbc6b92e
Kyle Mathews <mathews.kyle@gmail.com>
2016-07-14 19:06:04 -0700

    Add new 'Kirkham' theme

---
+++ packages/typography-theme-kirkham/src/index.js
@@ -0,0 +1,77 @@
+// @flow
+import gray from 'gray-percentage'
+import type { OptionsType } from 'Types'
+import { MOBILE_MEDIA_QUERY, TABLET_MEDIA_QUERY } from 'typography-breakpoint-constants'
+
+const theme: OptionsType = {
+  baseFontSize: '18px',
+  baseLineHeight: '30px',
+  googleFonts: [
+    {
+      name: 'Playfair Display',
+      styles: [
+        '700',
+      ],
+    },
+    {
+      name: 'Fira Sans',
+      styles: [
+        '400',
+        '400i',
+        '700',
+        '700i',
+      ],
+    },
+  ],
+  headerFontFamily: ['Playfair Display', 'serif'],
+  bodyFontFamily: ['Fira Sans', 'sans-serif'],
+  headerGray: 0,
+  bodyGray: 20,
+  headerWeight: 700,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options) => ({
+    a: {
+      color: '#9f392b',
+    },
+    h1: {
+      ...adjustFontSizeTo('48px'),
+    },
+    blockquote: {
+      ...adjustFontSizeTo('21px'),
+      color: gray(41),
+      fontStyle: 'italic',
+      paddingLeft: rhythm(13/16),
+      marginLeft: 0,
+      borderLeft: `${rhythm(3/16)} solid ${gray(80)}`,
+    },
+    'blockquote > :last-child': {
+      marginBottom: 0,
+    },
+    'blockquote cite': {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: gray(options.bodyGray),
+      fontStyle: options.bodyWeight,
+    },
+    'blockquote cite:before': {
+      content: '"— "',
+    },
+    [MOBILE_MEDIA_QUERY]: {
+      blockquote: {
+        marginLeft: rhythm(-3/4),
+        marginRight: 0,
+        paddingLeft: rhythm(9/16),
+      },
+    },
+    [TABLET_MEDIA_QUERY]: {
+      h1: {
+        ...adjustFontSizeTo('36px'),
+      },
+    },
+    'h3,h4,h5,h6': {
+      fontStyle: 'italic',
+    },
+  }),
+}
+
+export default theme
