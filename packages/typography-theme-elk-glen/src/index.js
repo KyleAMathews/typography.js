6ecd57e0dc0b7a358ce13298bbe338a6680a1855
Kyle Mathews <mathews.kyle@gmail.com>
2016-07-14 21:18:41 -0700

    Add new theme 'Elk Glen'

---
+++ packages/typography-theme-elk-glen/src/index.js
@@ -0,0 +1,82 @@
+// @flow
+import gray from 'gray-percentage'
+import type { OptionsType } from 'Types'
+import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants'
+import verticalRhythm from 'compass-vertical-rhythm'
+
+const theme: OptionsType = {
+  baseFontSize: '20px',
+  baseLineHeight: '33px',
+  googleFonts: [
+    {
+      name: 'Alegreya Sans',
+      styles: [
+        '500',
+      ],
+    },
+    {
+      name: 'Alegreya',
+      styles: [
+        '400',
+        '400i',
+        '700',
+        '700i',
+      ],
+    },
+  ],
+  headerFontFamily: ['Alegreya Sans', 'sans-serif'],
+  bodyFontFamily: ['Alegreya', 'sans-serif'],
+  headerGray: 10,
+  bodyGray: 20,
+  headerWeight: 500,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options) => {
+    const vr = verticalRhythm({
+      baseFontSize: '18px',
+      baseLineHeight: '29.7px',
+    })
+
+    return {
+      a: {
+        color: 'hsl(230,55%,58%)',
+        textDecoration: 'none',
+      },
+      'a:hover,a:active': {
+        boxShadow: '0 1px 0 0 currentColor',
+      },
+      blockquote: {
+        ...adjustFontSizeTo('21px'),
+        color: gray(35),
+        fontStyle: 'italic',
+        paddingLeft: rhythm(11/16),
+        marginLeft: 0,
+        borderLeft: `${rhythm(5/16)} solid ${gray(80)}`,
+      },
+      'blockquote > :last-child': {
+        marginBottom: 0,
+      },
+      'blockquote cite': {
+        ...adjustFontSizeTo(options.baseFontSize),
+        color: gray(options.bodyGray),
+        fontStyle: options.bodyWeight,
+      },
+      'blockquote cite:before': {
+        content: '"— "',
+      },
+      [MOBILE_MEDIA_QUERY]: {
+        html: {
+          ...vr.establishBaseline(),
+        },
+        blockquote: {
+          borderLeft: `${rhythm(3/16)} solid ${gray(80)}`,
+          marginLeft: rhythm(-3/4),
+          marginRight: 0,
+          paddingLeft: rhythm(9/16),
+        },
+      },
+    }
+  },
+}
+
+export default theme
