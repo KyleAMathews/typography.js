b8e4936a9382b1a8b4a2bfb736791bc30d4cac00
Kyle Mathews <mathews.kyle@gmail.com>
2016-07-15 12:13:11 -0700

    Add new theme 'Grand View'

---
+++ packages/typography-theme-grand-view/src/index.js
@@ -0,0 +1,72 @@
+// @flow
+import gray from 'gray-percentage'
+import type { OptionsType } from 'Types'
+import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants'
+import verticalRhythm from 'compass-vertical-rhythm'
+
+const theme: OptionsType = {
+  baseFontSize: '16px',
+  baseLineHeight: '27px',
+  googleFonts: [
+    {
+      name: 'Montserrat',
+      styles: [
+        '700',
+      ],
+    },
+    {
+      name: 'Arvo',
+      styles: [
+        '400',
+        '400i',
+        '700',
+      ],
+    },
+  ],
+  headerFontFamily: ['Montserrat', 'sans-serif'],
+  bodyFontFamily: ['Arvo', 'sans-serif'],
+  headerGray: 0,
+  bodyGray: 20,
+  headerWeight: 700,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options) => ({
+    a: {
+      color: '#d65947',
+      textDecoration: 'none',
+    },
+    'a:hover,a:active': {
+      color: gray(options.bodyGray),
+    },
+    'h1,h2,h3,h4,h5,h6': {
+      marginTop: rhythm(2),
+    },
+    blockquote: {
+      ...adjustFontSizeTo('19px'),
+      color: gray(41),
+      paddingLeft: rhythm(13/16),
+      marginLeft: 0,
+      borderLeft: `${rhythm(3/16)} solid #fca206`,
+    },
+    'blockquote > :last-child': {
+      marginBottom: 0,
+    },
+    'blockquote cite': {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: gray(options.bodyGray),
+      fontStyle: options.bodyWeight,
+    },
+    'blockquote cite:before': {
+      content: '"— "',
+    },
+    [MOBILE_MEDIA_QUERY]: {
+      blockquote: {
+        marginLeft: rhythm(-3/4),
+        marginRight: 0,
+        paddingLeft: rhythm(9/16),
+      },
+    },
+  }),
+}
+
+export default theme
