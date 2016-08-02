5657e0bd484519648b8bc3f3fd1a9fd76a5c7370
Kyle Mathews <mathews.kyle@gmail.com>
2016-08-01 19:14:08 -0600

    Swap Elk Glen and de Young themes so de Young theme more befitting art museum

--- packages/typography-theme-de-young/src/index.js
+++ packages/typography-theme-de-young/src/index.js
@@ -7,59 +7,52 @@ import verticalRhythm from 'compass-vertical-rhythm'
 const theme: OptionsType = {
   title: 'de Young',
   baseFontSize: '20px',
-  baseLineHeight: '30px',
+  baseLineHeight: '33px',
   googleFonts: [
     {
-      name: 'Oswald',
+      name: 'Alegreya Sans',
       styles: [
-        '700',
+        '500',
       ],
     },
     {
-      name: 'PT Sans',
+      name: 'Alegreya',
       styles: [
         '400',
         '400i',
         '700',
+        '700i',
       ],
     },
   ],
-  headerFontFamily: ['Oswald', 'sans-serif'],
-  bodyFontFamily: ['PT Sans', 'sans-serif'],
+  headerFontFamily: ['Alegreya Sans', 'sans-serif'],
+  bodyFontFamily: ['Alegreya', 'sans-serif'],
   headerGray: 10,
   bodyGray: 20,
-  headerWeight: '700',
+  headerWeight: 500,
   bodyWeight: 400,
   boldWeight: 700,
   overrideStyles: ({ adjustFontSizeTo, adjustFontSizeToMSValue, rhythm }, options) => {
-    const linkColor = '#292d35'
     const vr = verticalRhythm({
-      baseFontSize: '17px',
-      baseLineHeight: '24.65px',
+      baseFontSize: '18px',
+      baseLineHeight: '29.7px',
     })
+
     return {
       a: {
-        color: linkColor,
+        color: 'hsl(230,55%,58%)',
         textDecoration: 'none',
-        textShadow: '.03em 0 #fff,-.03em 0 #fff,0 .03em #fff,0 -.03em #fff,.06em 0 #fff,-.06em 0 #fff,.09em 0 #fff,-.09em 0 #fff,.12em 0 #fff,-.12em 0 #fff,.15em 0 #fff,-.15em 0 #fff', // eslint-disable-line
-        backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0) 1px, ${linkColor} 1px, ${linkColor} 2px, rgba(0, 0, 0, 0) 2px)`, // eslint-disable-line
       },
       'a:hover,a:active': {
-        textShadow: 'none',
-        backgroundImage: 'none',
+        boxShadow: '0 1px 0 0 currentColor',
       },
-      'h1,h2,h3,h4,h5,h6': {
-        marginTop: rhythm(2),
-      },
-      // Blockquote styles.
       blockquote: {
         ...adjustFontSizeToMSValue(1/5),
-        borderLeft: `${rhythm(6/16)} solid ${linkColor}`,
         color: gray(35),
-        paddingLeft: rhythm(10/16),
         fontStyle: 'italic',
+        paddingLeft: rhythm(11/16),
         marginLeft: 0,
-        marginRight: 0,
+        borderLeft: `${rhythm(5/16)} solid ${gray(80)}`,
       },
       'blockquote > :last-child': {
         marginBottom: 0,
@@ -67,7 +60,6 @@ const theme: OptionsType = {
       'blockquote cite': {
         ...adjustFontSizeTo(options.baseFontSize),
         color: gray(options.bodyGray),
-        fontStyle: 'normal',
         fontWeight: options.bodyWeight,
       },
       'blockquote cite:before': {
@@ -78,12 +70,10 @@ const theme: OptionsType = {
           ...vr.establishBaseline(),
         },
         blockquote: {
-          borderLeft: `${rhythm(3/16)} solid ${linkColor}`,
-          color: gray(41),
-          paddingLeft: rhythm(9/16),
-          fontStyle: 'italic',
+          borderLeft: `${rhythm(3/16)} solid ${gray(80)}`,
           marginLeft: rhythm(-3/4),
           marginRight: 0,
+          paddingLeft: rhythm(9/16),
         },
       },
     }
--- packages/typography-theme-elk-glen/src/index.js
+++ packages/typography-theme-elk-glen/src/index.js
@@ -7,52 +7,59 @@ import verticalRhythm from 'compass-vertical-rhythm'
 const theme: OptionsType = {
   title: 'Elk Glen',
   baseFontSize: '20px',
-  baseLineHeight: '33px',
+  baseLineHeight: '30px',
   googleFonts: [
     {
-      name: 'Alegreya Sans',
+      name: 'Oswald',
       styles: [
-        '500',
+        '700',
       ],
     },
     {
-      name: 'Alegreya',
+      name: 'PT Sans',
       styles: [
         '400',
         '400i',
         '700',
-        '700i',
       ],
     },
   ],
-  headerFontFamily: ['Alegreya Sans', 'sans-serif'],
-  bodyFontFamily: ['Alegreya', 'sans-serif'],
+  headerFontFamily: ['Oswald', 'sans-serif'],
+  bodyFontFamily: ['PT Sans', 'sans-serif'],
   headerGray: 10,
   bodyGray: 20,
-  headerWeight: 500,
+  headerWeight: '700',
   bodyWeight: 400,
   boldWeight: 700,
   overrideStyles: ({ adjustFontSizeTo, adjustFontSizeToMSValue, rhythm }, options) => {
+    const linkColor = '#292d35'
     const vr = verticalRhythm({
-      baseFontSize: '18px',
-      baseLineHeight: '29.7px',
+      baseFontSize: '17px',
+      baseLineHeight: '24.65px',
     })
-
     return {
       a: {
-        color: 'hsl(230,55%,58%)',
+        color: linkColor,
         textDecoration: 'none',
+        textShadow: '.03em 0 #fff,-.03em 0 #fff,0 .03em #fff,0 -.03em #fff,.06em 0 #fff,-.06em 0 #fff,.09em 0 #fff,-.09em 0 #fff,.12em 0 #fff,-.12em 0 #fff,.15em 0 #fff,-.15em 0 #fff', // eslint-disable-line
+        backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0) 1px, ${linkColor} 1px, ${linkColor} 2px, rgba(0, 0, 0, 0) 2px)`, // eslint-disable-line
       },
       'a:hover,a:active': {
-        boxShadow: '0 1px 0 0 currentColor',
+        textShadow: 'none',
+        backgroundImage: 'none',
       },
+      'h1,h2,h3,h4,h5,h6': {
+        marginTop: rhythm(2),
+      },
+      // Blockquote styles.
       blockquote: {
         ...adjustFontSizeToMSValue(1/5),
+        borderLeft: `${rhythm(6/16)} solid ${linkColor}`,
         color: gray(35),
+        paddingLeft: rhythm(10/16),
         fontStyle: 'italic',
-        paddingLeft: rhythm(11/16),
         marginLeft: 0,
-        borderLeft: `${rhythm(5/16)} solid ${gray(80)}`,
+        marginRight: 0,
       },
       'blockquote > :last-child': {
         marginBottom: 0,
@@ -60,6 +67,7 @@ const theme: OptionsType = {
       'blockquote cite': {
         ...adjustFontSizeTo(options.baseFontSize),
         color: gray(options.bodyGray),
+        fontStyle: 'normal',
         fontWeight: options.bodyWeight,
       },
       'blockquote cite:before': {
@@ -70,10 +78,12 @@ const theme: OptionsType = {
           ...vr.establishBaseline(),
         },
         blockquote: {
-          borderLeft: `${rhythm(3/16)} solid ${gray(80)}`,
+          borderLeft: `${rhythm(3/16)} solid ${linkColor}`,
+          color: gray(41),
+          paddingLeft: rhythm(9/16),
+          fontStyle: 'italic',
           marginLeft: rhythm(-3/4),
           marginRight: 0,
-          paddingLeft: rhythm(9/16),
         },
       },
     }
