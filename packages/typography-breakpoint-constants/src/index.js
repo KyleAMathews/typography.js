7441d71e7848d76ebc2f6c2b3d991e65354a88be
Damir <damir@withpulp.com>
2016-08-07 22:14:48 -0400

    added min-width media queries, added media query for 1600px

--- packages/typography-breakpoint-constants/src/index.js
+++ packages/typography-breakpoint-constants/src/index.js
@@ -1,18 +1,17 @@
+export const LARGER_DISPLAY_WIDTH = '1600px'
 export const LARGE_DISPLAY_WIDTH = '1280px'
 export const DEFAULT_WIDTH = '980px'
 export const TABLET_WIDTH = '768px'
 export const MOBILE_WIDTH = '480px'
 
+export const LARGER_DISPLAY_MEDIA_QUERY = '@media only screen and (max-width:1600px)'
 export const LARGE_DISPLAY_MEDIA_QUERY = '@media only screen and (max-width:1280px)'
 export const DEFAULT_MEDIA_QUERY = '@media only screen and (max-width:980px)'
 export const TABLET_MEDIA_QUERY = '@media only screen and (max-width:768px)'
 export const MOBILE_MEDIA_QUERY = '@media only screen and (max-width:480px)'
 
-// mobile first media queries:
-// define css rules for mobile first
-// add additional css rules inside the min_width media query as the device/display width increases
-// to overwrite the mobile styles (easier to scale this way)
-export const MIN_LARGE_DISPLAY_WIDTH = '@media (min-width:1280px)'
-export const MIN_DEFAULT_WIDTH = '@media (min-width:980px)'
-export const MIN_TABLET_WIDTH = '@media (min-width:768px)'
-export const MIN_MOBILE_WIDTH = '@media (min-width:480px)'
+export const MIN_LARGER_DISPLAY_MEDIA_QUERY = '@media (min-width:1600px)'
+export const MIN_LARGE_DISPLAY_MEDIA_QUERY = '@media (min-width:1280px)'
+export const MIN_DEFAULT_MEDIA_QUERY = '@media (min-width:980px)'
+export const MIN_TABLET_MEDIA_QUERY = '@media (min-width:768px)'
+export const MIN_MOBILE_MEDIA_QUERY = '@media (min-width:480px)'
