07c5b80697a5ed10556a113477b5a816f3872932
Kyle Mathews <mathews.kyle@gmail.com>
2017-07-07 09:37:21 -0700

    Quote font names so multi-word fonts work

--- packages/typography/src/utils/createStyles.js
+++ packages/typography/src/utils/createStyles.js
@@ -1,24 +1,28 @@
 // @flow
-import gray from 'gray-percentage'
-import set from 'lodash/set'
-import each from 'lodash/forEach'
-import isNumber from 'lodash/isNumber'
-import isString from 'lodash/isString'
-import isFunction from 'lodash/isFunction'
-import isArray from 'lodash/isArray'
-import merge from 'lodash/merge'
-import reduce from 'lodash/reduce'
+import gray from "gray-percentage"
+import set from "lodash/set"
+import each from "lodash/forEach"
+import isNumber from "lodash/isNumber"
+import isString from "lodash/isString"
+import isFunction from "lodash/isFunction"
+import isArray from "lodash/isArray"
+import merge from "lodash/merge"
+import reduce from "lodash/reduce"
 
-import type { OptionsType } from 'Types'
+import type { OptionsType } from "Types"
 
-const setStyles = (styles: Object = {}, els: string | string[], rules: Object) => {
+const setStyles = (
+  styles: Object = {},
+  els: string | string[],
+  rules: Object
+) => {
   let elements
   if (!isArray(els)) {
     elements = [els]
   } else {
     elements = els
   }
-  each(elements, (element) => {
+  each(elements, element => {
     each(rules, (value, prop) => {
       set(styles, `${element}.${prop}`, value)
     })
@@ -31,24 +35,26 @@ module.exports = (vr: any, options: OptionsType) => {
   const { fontSize, lineHeight } = vr.establishBaseline()
 
   // Base HTML styles.
-  styles = setStyles(styles, 'html', {
-    font: `${fontSize}/${lineHeight} ${options.bodyFontFamily.join(',')}`,
-    boxSizing: 'border-box',
-    overflowY: 'scroll',
+  styles = setStyles(styles, "html", {
+    font: `${fontSize}/${lineHeight} ${options.bodyFontFamily
+      .map(f => `'${f}'`)
+      .join(",")}`,
+    boxSizing: "border-box",
+    overflowY: "scroll",
   })
 
   // box-sizing reset.
-  styles = setStyles(styles, ['*', '*:before', '*:after'], {
-    boxSizing: 'inherit',
+  styles = setStyles(styles, ["*", "*:before", "*:after"], {
+    boxSizing: "inherit",
   })
 
   // Base body styles.
-  styles = setStyles(styles, 'body', {
+  styles = setStyles(styles, "body", {
     color: options.bodyColor,
-    fontFamily: options.bodyFontFamily.join(','),
+    fontFamily: options.bodyFontFamily.map(f => `'${f}'`).join(","),
     fontWeight: options.bodyWeight,
-    wordWrap: 'break-word',
-    fontKerning: 'normal',
+    wordWrap: "break-word",
+    fontKerning: "normal",
     MozFontFeatureSettings: '"kern", "liga", "clig", "calt"',
     msFontFeatureSettings: '"kern", "liga", "clig", "calt"',
     WebkitFontFeatureSettings: '"kern", "liga", "clig", "calt"',
@@ -56,13 +62,13 @@ module.exports = (vr: any, options: OptionsType) => {
   })
 
   // Make images responsive.
-  styles = setStyles(styles, 'img', {
-    maxWidth: '100%',
+  styles = setStyles(styles, "img", {
+    maxWidth: "100%",
   })
 
   // All block elements get one rhythm of bottom margin by default
   // or whatever is passed in as option.
-  let blockMarginBottom = ''
+  let blockMarginBottom = ""
   if (isNumber(options.blockMarginBottom)) {
     blockMarginBottom = vr.rhythm(options.blockMarginBottom)
   } else if (isString(options.blockMarginBottom)) {
@@ -70,82 +76,85 @@ module.exports = (vr: any, options: OptionsType) => {
   } else {
     blockMarginBottom = vr.rhythm(1)
   }
-  styles = setStyles(styles, [
-    'h1',
-    'h2',
-    'h3',
-    'h4',
-    'h5',
-    'h6',
-    'hgroup',
-    'ul',
-    'ol',
-    'dl',
-    'dd',
-    'p',
-    'figure',
-    'pre',
-    'table',
-    'fieldset',
-    'blockquote',
-    'form',
-    'noscript',
-    'iframe',
-    'img',
-    'hr',
-    'address',
-  ], {
-
-    // Reset margin/padding to 0.
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 0,
-    paddingBottom: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-    marginBottom: blockMarginBottom,
-  })
+  styles = setStyles(
+    styles,
+    [
+      "h1",
+      "h2",
+      "h3",
+      "h4",
+      "h5",
+      "h6",
+      "hgroup",
+      "ul",
+      "ol",
+      "dl",
+      "dd",
+      "p",
+      "figure",
+      "pre",
+      "table",
+      "fieldset",
+      "blockquote",
+      "form",
+      "noscript",
+      "iframe",
+      "img",
+      "hr",
+      "address",
+    ],
+    {
+      // Reset margin/padding to 0.
+      marginLeft: 0,
+      marginRight: 0,
+      marginTop: 0,
+      paddingBottom: 0,
+      paddingLeft: 0,
+      paddingRight: 0,
+      paddingTop: 0,
+      marginBottom: blockMarginBottom,
+    }
+  )
 
   // Basic blockquote styles.
-  styles = setStyles(styles, 'blockquote', {
+  styles = setStyles(styles, "blockquote", {
     marginRight: vr.rhythm(1),
     marginBottom: blockMarginBottom,
     marginLeft: vr.rhythm(1),
   })
 
   // b, strong.
-  styles = setStyles(styles, ['b', 'strong', 'dt', 'th'], {
+  styles = setStyles(styles, ["b", "strong", "dt", "th"], {
     fontWeight: options.boldWeight,
   })
 
   // hr.
-  styles = setStyles(styles, 'hr', {
+  styles = setStyles(styles, "hr", {
     background: gray(80),
-    border: 'none',
-    height: '1px',
+    border: "none",
+    height: "1px",
     marginBottom: `calc(${blockMarginBottom} - 1px)`,
   })
 
   // ol, ul.
-  styles = setStyles(styles, ['ol', 'ul'], {
-    listStylePosition: 'outside',
-    listStyleImage: 'none',
+  styles = setStyles(styles, ["ol", "ul"], {
+    listStylePosition: "outside",
+    listStyleImage: "none",
     marginLeft: vr.rhythm(1),
   })
 
   // li.
-  styles = setStyles(styles, 'li', {
+  styles = setStyles(styles, "li", {
     marginBottom: `calc(${blockMarginBottom} / 2)`,
   })
 
   // Remove default padding on list items.
-  styles = setStyles(styles, ['ol li', 'ul li'], {
+  styles = setStyles(styles, ["ol li", "ul li"], {
     paddingLeft: 0,
   })
 
   // children ol, ul.
-  styles = setStyles(styles, ['li > ol', 'li > ul'], {
+  styles = setStyles(styles, ["li > ol", "li > ul"], {
     marginLeft: vr.rhythm(1),
     marginBottom: `calc(${blockMarginBottom} / 2)`,
     marginTop: `calc(${blockMarginBottom} / 2)`,
@@ -154,80 +163,78 @@ module.exports = (vr: any, options: OptionsType) => {
   // Remove margin-bottom on the last-child of a few block elements
   // The worst offender of this seems to be markdown => html compilers
   // as they put paragraphs within LIs amoung other oddities.
-  styles = setStyles(styles, [
-    'blockquote *:last-child',
-    'li *:last-child',
-    'p *:last-child',
-  ], {
-    marginBottom: 0,
-  })
+  styles = setStyles(
+    styles,
+    ["blockquote *:last-child", "li *:last-child", "p *:last-child"],
+    {
+      marginBottom: 0,
+    }
+  )
 
   // Ensure li > p is 1/2 margin — this is another markdown => compiler oddity.
-  styles = setStyles(styles, [
-    'li > p',
-  ], {
+  styles = setStyles(styles, ["li > p"], {
     marginBottom: `calc(${blockMarginBottom} / 2)`,
   })
 
   // Make generally smaller elements, smaller.
-  styles = setStyles(styles, ['code', 'kbd', 'pre', 'samp'], {
-    ...vr.adjustFontSizeTo('85%'),
+  styles = setStyles(styles, ["code", "kbd", "pre", "samp"], {
+    ...vr.adjustFontSizeTo("85%"),
   })
 
   // Abbr, Acronym.
-  styles = setStyles(styles, ['abbr', 'acronym'], {
+  styles = setStyles(styles, ["abbr", "acronym"], {
     borderBottom: `1px dotted ${gray(50)}`,
-    cursor: 'help',
+    cursor: "help",
   })
-  styles['abbr[title]'] = {
+  styles["abbr[title]"] = {
     borderBottom: `1px dotted ${gray(50)}`,
-    cursor: 'help',
-    textDecoration: 'none',
+    cursor: "help",
+    textDecoration: "none",
   }
 
   // Table styles.
-  styles = setStyles(styles, ['table'], {
+  styles = setStyles(styles, ["table"], {
     ...vr.adjustFontSizeTo(options.baseFontSize),
-    borderCollapse: 'collapse',
-    width: '100%',
+    borderCollapse: "collapse",
+    width: "100%",
   })
-  styles = setStyles(styles, ['thead'], {
-    textAlign: 'left',
+  styles = setStyles(styles, ["thead"], {
+    textAlign: "left",
   })
-  styles = setStyles(styles, ['td,th'], {
-    textAlign: 'left',
+  styles = setStyles(styles, ["td,th"], {
+    textAlign: "left",
     borderBottom: `1px solid ${gray(88)}`,
     fontFeatureSettings: '"tnum"',
     MozFontFeatureSettings: '"tnum"',
     msFontFeatureSettings: '"tnum"',
     WebkitFontFeatureSettings: '"tnum"',
-    paddingLeft: vr.rhythm(2/3),
-    paddingRight: vr.rhythm(2/3),
-    paddingTop: vr.rhythm(1/2),
-    paddingBottom: `calc(${vr.rhythm(1/2)} - 1px)`,
+    paddingLeft: vr.rhythm(2 / 3),
+    paddingRight: vr.rhythm(2 / 3),
+    paddingTop: vr.rhythm(1 / 2),
+    paddingBottom: `calc(${vr.rhythm(1 / 2)} - 1px)`,
   })
-  styles = setStyles(styles, 'th:first-child,td:first-child', {
+  styles = setStyles(styles, "th:first-child,td:first-child", {
     paddingLeft: 0,
   })
-  styles = setStyles(styles, 'th:last-child,td:last-child', {
+  styles = setStyles(styles, "th:last-child,td:last-child", {
     paddingRight: 0,
   })
 
   // Create styles for headers.
-  styles = setStyles(styles, ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'], {
+  styles = setStyles(styles, ["h1", "h2", "h3", "h4", "h5", "h6"], {
     color: options.headerColor,
-    fontFamily: options.headerFontFamily.join(','),
+    fontFamily: options.headerFontFamily.map(f => `'${f}'`).join(","),
     fontWeight: options.headerWeight,
-    textRendering: 'optimizeLegibility',
+    textRendering: "optimizeLegibility",
   })
 
   // Set header sizes.
-  const h1 = vr.scale(5/5)
-  const h2 = vr.scale(3/5)
-  const h3 = vr.scale(2/5)
-  const h4 = vr.scale(0/5)
-  const h5 = vr.scale(-1/5)
-  const h6 = vr.scale(-1.5/5)
+  const h1 = vr.scale(5 / 5)
+  const h2 = vr.scale(3 / 5)
+  const h3 = vr.scale(2 / 5)
+  const h4 = vr.scale(0 / 5)
+  const h5 = vr.scale(-1 / 5)
+  const h6 = vr.scale(-1.5 / 5)
 
   each([h1, h2, h3, h4, h5, h6], (header, i) => {
     styles = set(styles, `h${i + 1}.fontSize`, header.fontSize)
@@ -238,9 +245,11 @@ module.exports = (vr: any, options: OptionsType) => {
 
   // Call plugins if any.
   if (isArray(options.plugins)) {
-    styles = reduce(options.plugins, ((stylesObj, plugin) => (
-      merge(stylesObj, plugin(vr, options, stylesObj))
-    )), styles)
+    styles = reduce(
+      options.plugins,
+      (stylesObj, plugin) => merge(stylesObj, plugin(vr, options, stylesObj)),
+      styles
+    )
   }
 
   // Call overrideStyles function on options (if set).
