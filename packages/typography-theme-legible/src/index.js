c4e02528396cfaaa7f97224117a949b6f7f03f7b
Esa Juhana <info@esajuhana.com>
2017-11-02 19:24:24 +0200

    Add Legible theme (#137)
    Adds legible theme, that uses Black Merriweather and Fira Sans
    with somewhat generous spacing.

---
+++ packages/typography-theme-legible/src/index.js
@@ -0,0 +1,28 @@
+// @flow
+import type { OptionsType } from '../../../Types'
+
+const theme: OptionsType = {
+  title: 'Legible',
+  baseFontSize: '16px',
+  baseLineHeight: 1.40,
+  googleFonts: [
+    {
+      name: 'Fira Sans',
+      styles: ['400', '500'],
+    },
+    {
+      name: 'Merriweather',
+      styles: ['900'],
+    },
+  ],
+  headerFontFamily: ['Merriweather', 'serif'],
+  bodyFontFamily: ['Fira Sans', 'sans-serif'],
+  headerColor: 'hsla(0, 0%, 0%, 0.86)',
+  bodyColor: 'hsla(0, 0%, 0%, 0.93)',
+  headerWeight: 900,
+  bodyWeight: 400,
+  boldWeight: 500,
+  scaleRatio: 2.45
+}
+
+export default theme
