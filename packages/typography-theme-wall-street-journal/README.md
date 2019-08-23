a282fa5ab63e6ede7f53788b07ba6ac2a4c7535a
Ruben Profit <rubenprofit@ac.c4q.nyc>
2019-08-22 22:25:22 -0400

    typography theme wall street journal

---
+++ packages/typography-theme-wall-street-journal/README.md
@@ -0,0 +1,18 @@
+# Github — a <a href='https://github.com/kyleamathews/typography.js'>Typography.js</a> theme
+
+A theme for Typography.js inspired by The Wall Street Journal's typography
+
+## Install
+```bash
+npm install --save typography typography-theme-wall-street-journal
+```
+## Usage
+```javascript
+import Typography from 'typography'
+import wsjTheme from 'typography-theme-wall-street-journal'
+
+const typography = new Typography(wsjTheme)
+```
+## More on theme
+
+Github uses "Escrow" for headers and "Exchange" for body text
---
+++ packages/typography-theme-wall-street-journal/src/index.js
@@ -0,0 +1,62 @@
+// @flow
+import gray from "gray-percentage"
+import type { OptionsType } from "Types"
+
+const theme: OptionsType = {
+  title: "Wall Street Journal",
+  baseFontSize: "20px",
+  baseLineHeight: 1.625,
+  headerFontFamily: ["escrow", "serif"],
+  bodyFontFamily: ["escrow", "serif"],
+  scaleRatio: 2,
+  bodyColor: "hsla(0,0%,0%,0.8)",
+  headerWeight: 600,
+  bodyWeight: "normal",
+  boldWeight: 600,
+  blockMarginBottom: 1 / 2,
+  overrideStyles: ({ rhythm }) => ({
+    h1: {
+      borderBottom: `1px solid ${gray(93)}`,
+      paddingBottom: `calc(${rhythm(1 / 4)} - 1px)`,
+      marginBottom: rhythm(3 / 4),
+      marginTop: rhythm(1.5),
+    },
+    h2: {
+      borderBottom: `1px solid ${gray(93)}`,
+      paddingBottom: `calc(${rhythm(1 / 4)} - 1px)`,
+      marginBottom: rhythm(1 / 4),
+      marginTop: rhythm(1),
+    },
+    h6: {
+      color: gray(47),
+    },
+    "h3,h4,h5,h6": {
+      marginBottom: rhythm(1 / 2),
+      marginTop: rhythm(1),
+    },
+    "ol,ul": {
+      marginLeft: rhythm(1.25),
+    },
+    // children ol, ul
+    "li>ol,li>ul": {
+      marginLeft: rhythm(1.25),
+    },
+    a: {
+      color: "#4078c0",
+      textDecoration: "none",
+    },
+    "a:hover,a:active": {
+      textDecoration: "underline",
+    },
+    blockquote: {
+      borderLeft: `4px solid ${gray(87)}`,
+      color: gray(47),
+      marginTop: 0,
+      marginRight: 0,
+      marginLeft: 0,
+      paddingLeft: `calc(${rhythm(1 / 2)} - 1px)`,
+    },
+  }),
+}
+
+export default theme
